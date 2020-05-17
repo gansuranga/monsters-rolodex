437c5856b1b8cdf57a3671f17d429029bc839a57
c5@granbymarketing.com <gan.suranga@gmail.com>
2020-05-17 14:34:36 +0100

    Updates

---
+++ precache-manifest.901eb7553a293edbb053c90e25303c1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d41d2262524a216e2df107fc7f31c587",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e92727c5f1604c37b4cc",
+    "url": "/monsters-rolodex/static/css/main.e8e64c36.chunk.css"
+  },
+  {
+    "revision": "f7b84cd810271619eca9",
+    "url": "/monsters-rolodex/static/js/2.7d94dc6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7d94dc6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e92727c5f1604c37b4cc",
+    "url": "/monsters-rolodex/static/js/main.f45e08dc.chunk.js"
+  },
+  {
+    "revision": "e3f9453037e5f28f8167",
+    "url": "/monsters-rolodex/static/js/runtime-main.7c0f1f0f.js"
+  }
+]);
